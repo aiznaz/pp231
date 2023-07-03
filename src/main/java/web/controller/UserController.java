212787f2c7335c40e71995db49cdc37cee6809e7
aiznaz <131270076+aiznaz@users.noreply.github.com>
2023-07-03 13:26:21 +0300

    1ыt try

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,53 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.UserService;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    public String showUsersTable(Model model){
+        model.addAttribute("users", userService.getUsersList());
+        return "users";
+    }
+
+    @GetMapping("/new")
+    public String createNewUser(@ModelAttribute("user") User user) {
+        return "new_user";
+    }
+
+    @PostMapping("/")
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.addUser(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String editUser(Model model, @PathVariable("id") int id) {
+        model.addAttribute("user", userService.getUser(id));
+        return "edit_user";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.editUser(user);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteUser(@PathVariable("id") int id) {
+        userService.deleteUser(id);
+        return "redirect:/";
+    }
+
+}
